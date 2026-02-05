479ec5e905d82b8c84b39ed0ef62eb24b0e984ef
workaccount3 <143459191+workaccount3@users.noreply.github.com>
2026-02-05 14:35:25 -0500

    added binary search to verifyAccess

--- Project 1/UserAccessManager.java
+++ Project 1/UserAccessManager.java
@@ -4,10 +4,13 @@ import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 
-public class UserAccessManager {
+public class UserAccessManager
+{
+	private final boolean isLocked = true;
 	private List<UserAccount> Accounts;
 	
-	public UserAccessManager() {
+	public UserAccessManager()
+	{
 		Accounts = new ArrayList<>(); 
 	}
 
@@ -19,12 +22,15 @@ public class UserAccessManager {
 	 * @param filename 
 	 * @throws FileNotFoundException
 	 */
-	public void loadAccounts(String filename)throws FileNotFoundException{
-		try {		
+	public void loadAccounts(String filename)throws FileNotFoundException
+	{
+		try 
+		{		
 			File dataFile= new File(filename);
 			Scanner reader = new Scanner(dataFile);
 			
-			while(reader.hasNextLine()) {
+			while(reader.hasNextLine()) 
+			{
 				//https://www.w3schools.com/java/ref_string_split.asp
 				String regex = "\\s+";
 				String  data = reader.nextLine().trim();
@@ -38,13 +44,37 @@ public class UserAccessManager {
 				Accounts.add(userAccount);
 			}
 			reader.close();
-		}catch(FileNotFoundException e) {	
+		}
+		catch(FileNotFoundException e)
+		{	
 			System.out.println(e.getMessage());
-		}finally {
-			System.out.println("finished loading accounts.");
+		}
+		finally
+		{
+			System.out.println("Loading attempt finished.");
 		}
 			
 	}	
+	//https://www.geeksforgeeks.org/dsa/binary-insertion-sort/
+	/**How Does Binary insertion sort Work?
+
+    In the binary insertion sort mode, we divide the same members into two subarrays - filtered and unfiltered. The first element of the same members is in the organized subarray, and all other elements are unplanned.
+    Then we iterate from the second element to the last. In the repetition of the i-th, we make the current object our "key". This key is a feature that we should add to our existing list below.
+    In order to do this, we first use a binary search on the sorted subarray below to find the location of an element larger than our key. Let's call this position “pos.” We then right shift all the elements from pos to 1 and created Array[pos] = key.
+
+  
+	 */
+	
+	/**Approach to implement Binary Insertion sort:
+	*
+	*			Iterate the array from the second element to the last element.
+	*			Store the current element A[i] in a variable key.
+	*			Find the position of the element just greater than A[i] in the subarray from A[0] to A[i-1] using binary search. Say this element is at index pos.
+	*			Shift all the elements from index pos to i-1 towards the right.
+	*			A[pos] = key.
+    /
+	
+	
 	/**
 	 * This method adds users to the the UserAccessManager Accounts list.
 	 * 
@@ -55,11 +85,12 @@ public class UserAccessManager {
 	 */
 	public void addUser(String username, String encryptedPassword)throws DuplicateUserException,InvalidCommandException
 	{
-		
-		//use binary sort to insert username alphabetically
+				//use binary sort to insert username alphabetically
 		if(Accounts.contains(username))
 			throw new DuplicateUserException("Username already taken.");
 		
+		// or add to array and then sort lowest to highest lexicographically.
+	
 		Accounts.add(new UserAccount(username,encryptedPassword));
 	}
 
@@ -90,17 +121,52 @@ public class UserAccessManager {
 	 */
 	public boolean verifyAccess(String username, String encryptedPassword)throws UserNotFoundException,  AccountLockedException, InvalidCommandException
 	{
-		boolean canAccess = false;
+		
+		final boolean doesExist = false;
 		int low =0;
 		int high = Accounts.size() - 1;
 		
-		while(low<=high) {
-			int middle = low + (low+high)/2;
+		try {
+			while(low<=high) 
+			{
+				int middle = low + (high-low)/2;
+				UserAccount account = Accounts.get(middle);
+				String dataUsername = account.getUser();
+				
+				if(dataUsername.equals(username)) 
+				{	
+					
+					if(account.checkStatus() == isLocked)
+						throw new AccountLockedException("Account is locked. Try again in a few hours.");
+							
+				}
+				else if(dataUsername.compareTo(username) < 0)
+				{
+					low = middle + 1;
+				}
+				else 
+				{
+					high = middle - 1;
+				}	
+			}
 			
+			if(!doesExist)
+				throw new UserNotFoundException("Unfortunately, this given user does not exist.");
 			
+		}
+		catch(AccountLockedException e) 
+		{
+			System.out.println(e.getMessage());
+			return false;
+		}
+		catch(UserNotFoundException e) 
+		{
+			System.out.println(e.getMessage());
+			return false;
 		}
 		
 		
-		return canAccess;
+		return true;
 	}
+	
 }
