a1b3900bd39e1820c7b775dc27a8db832b33e54b
thien <143459191+SaulWorking@users.noreply.github.com>
2026-02-13 15:38:14 -0500

    Update OrderComparator.java

--- Project2/OrderComparator.java
+++ Project2/OrderComparator.java
@@ -4,20 +4,22 @@ public class OrderComparator implements Comparator<Order> {
 	private final int SECOND_IS_GREATER = -1;
 	private final int FAIL = 0;
 	
-	
-	
+				@Override
+				public int compare(Order o1, Order o2) {
+					if(o1.getDeadlineMinute() == o2.getDeadlineMinute()) {
+						    if(o1.getArrivalMinute() > o2.getArrivalMinute()) 
+							      return FIRST_IS_GREATER;
+			       else
+			          return SECOND_IS_GREATER;
+					}
+					
+					if(o1.getDeadlineMinute() > o2.getArrivalMinute()) 
+						   return FIRST_IS_GREATER;
+			  else
+					    return SECOND_IS_GREATER;
 
-	@Override
-	public int compare(Order o1, Order o2) {
-		if(o1.getDeadlineMinute() == o2.getDeadlineMinute()) {
-			if(o1.getArrivalMinute() > o2.getArrivalMinute()) 
-				return FIRST_IS_GREATER;
-		}
-		
-		if(o1.getDeadlineMinute() > o2.getArrivalMinute()) 
-			return FIRST_IS_GREATER;
-		
-		return FAIL;
-	}
 
+					return FAIL;
+			
+			 }
 }
